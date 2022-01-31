516ee40e8df4139e898ad60d3565d36e55aa3324
solidtm <richardebboh@gmail.com>
2022-01-31 11:55:48 +0100

    created StarterActivity.kt for onboarding process

---
+++ app/src/main/java/com/youverify/agent_app_android/view/activity/StarterActivity.kt
@@ -0,0 +1,12 @@
+package com.youverify.agent_app_android.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.youverify.agent_app_android.R
+
+class StarterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_starter)
+    }
+}
