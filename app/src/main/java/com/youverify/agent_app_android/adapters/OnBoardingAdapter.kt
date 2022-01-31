ddec12519b28ba7077bca849052705c2c9ffe402
solidtm <richardebboh@gmail.com>
2022-01-31 17:52:14 +0100

    Finished implementation of onboarding screens

--- app/src/main/java/com/youverify/agent_app_android/adapters/OnBoardingAdapter.kt
+++ app/src/main/java/com/youverify/agent_app_android/adapters/OnBoardingAdapter.kt
@@ -1,25 +1,29 @@
 package com.youverify.agent_app_android.adapters
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.youverify.agent_app_android.databinding.OnboardingItemsBinding
 import com.youverify.agent_app_android.model.OnBoardingItems
 
-class OnBoardingAdapter: RecyclerView.Adapter<OnBoardingAdapter.OnBoardingViewHolder>(){
+class OnBoardingAdapter(private val onboardingItems : ArrayList<OnBoardingItems>): RecyclerView.Adapter<OnBoardingAdapter.OnBoardingViewHolder>(){
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OnBoardingViewHolder {
-        TODO("Not yet implemented")
+        return OnBoardingViewHolder(OnboardingItemsBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false))
     }
 
     override fun onBindViewHolder(holder: OnBoardingViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.apply {
+            bind(onboardingItems[position])
+        }
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return onboardingItems.size
     }
 
-    inner class OnBoardingViewHolder(val binding: OnboardingItemsBinding):
+    inner class OnBoardingViewHolder(private val binding: OnboardingItemsBinding):
         RecyclerView.ViewHolder(binding.root){
 
         fun bind(item: OnBoardingItems){
--- app/src/main/java/com/youverify/agent_app_android/view/activity/OnBoardingActivity.kt
+++ app/src/main/java/com/youverify/agent_app_android/view/activity/OnBoardingActivity.kt
@@ -3,10 +3,58 @@ package com.youverify.agent_app_android.view.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.youverify.agent_app_android.R
+import com.youverify.agent_app_android.adapters.OnBoardingAdapter
+import com.youverify.agent_app_android.databinding.ActivityOnboardingBinding
+import com.youverify.agent_app_android.model.OnBoardingItems
+
+
+private lateinit var binding: ActivityOnboardingBinding
 
 class OnBoardingActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_onboarding)
+        binding = ActivityOnboardingBinding.inflate(layoutInflater)
+        supportActionBar?.hide()
+        setContentView(binding.root)
+
+        val onboardingViewPager = binding.onBoardingViewPager
+        onboardingViewPager.adapter = setOnBoardingItems()
+
+    }
+
+    private fun setOnBoardingItems(): OnBoardingAdapter {
+        val onboardingItems = arrayListOf<OnBoardingItems>()
+
+        val items1 = OnBoardingItems(
+            title = "Easily manage tasks",
+            description = "Slide right to accept a task or slide left to decline.",
+            image = R.drawable.ic_vp_image_one
+        )
+
+        val items2 = OnBoardingItems(
+            title = "Real-time tracking",
+            description = "The app identifies your location with the Geo-tag feature while on duty.",
+            image = R.drawable.ic_vp_image_two
+        )
+
+        val items3 = OnBoardingItems(
+            title = "Earn money",
+            description = "Smile to the bank when you work as our agent.",
+            image = R.drawable.ic_vp_image_three
+        )
+
+        val items4 = OnBoardingItems(
+            title = "Get started",
+            description = "To get your first task, choose your location to request for task around you.",
+            image = R.drawable.ic_vp_image_four
+        )
+
+        onboardingItems.add(items1)
+        onboardingItems.add(items2)
+        onboardingItems.add(items3)
+        onboardingItems.add(items4)
+
+        return OnBoardingAdapter(onboardingItems)
     }
 }
