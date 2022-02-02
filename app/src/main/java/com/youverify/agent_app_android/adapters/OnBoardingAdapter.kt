b559208edf33c8d9693ea7f3996749220eac4758
solidtm <richardebboh@gmail.com>
2022-02-02 15:10:49 +0100

    Onboarding progressbar fix

--- app/src/main/java/com/youverify/agent_app_android/adapters/OnBoardingAdapter.kt
+++ app/src/main/java/com/youverify/agent_app_android/adapters/OnBoardingAdapter.kt
@@ -2,13 +2,15 @@ package com.youverify.agent_app_android.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
+import com.youverify.agent_app_android.R
 import com.youverify.agent_app_android.databinding.OnboardingItemsBinding
 import com.youverify.agent_app_android.model.OnBoardingItems
 
 class OnBoardingAdapter(private val onboardingItems: ArrayList<OnBoardingItems>) :
-    RecyclerView.Adapter<OnBoardingAdapter.OnBoardingViewHolder>() {
+    RecyclerView.Adapter<OnBoardingAdapter.OnBoardingViewHolder>(){
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OnBoardingViewHolder {
         return OnBoardingViewHolder(
@@ -36,6 +38,23 @@ class OnBoardingAdapter(private val onboardingItems: ArrayList<OnBoardingItems>)
             binding.imageOnBoarding.setImageResource(item.image)
             //we want to remove the back button when the first onboarding item shows up in the viewpager.
             binding.onBoardBackButton.isVisible = item != onboardingItems[0]
+            //we want to change the various leves for the progressbar on different items
+            when(item){
+                onboardingItems[0] ->
+                    binding.onBoardProgressBar.progress = 25
+                    //add coresponding color
+                onboardingItems[1] ->
+                    binding.onBoardProgressBar.progress = 50
+                    //add coresponding color
+                onboardingItems[2] ->
+                    binding.onBoardProgressBar.progress = 75
+                   //add coresponding color
+                onboardingItems[3] ->
+                    binding.onBoardProgressBar.progress = 100
+                   //add coresponding color
+                else -> binding.onBoardProgressBar.progress = 0
+            }
+
 
         }
     }
