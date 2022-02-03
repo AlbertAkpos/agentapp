73f4d1994c5418bdd870ede1e061e8892d952c0f
solidtm <richardebboh@gmail.com>
2022-02-03 19:40:59 +0100

    adding functionality for skip, next and back button, in progress

--- app/src/main/java/com/youverify/agent_app_android/adapters/OnBoardingAdapter.kt
+++ app/src/main/java/com/youverify/agent_app_android/adapters/OnBoardingAdapter.kt
@@ -3,6 +3,7 @@ package com.youverify.agent_app_android.adapters
 import android.content.Context
 import android.content.Intent
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat.startActivity
 import androidx.core.view.isVisible
@@ -10,8 +11,10 @@ import androidx.recyclerview.widget.RecyclerView
 import com.youverify.agent_app_android.databinding.ActivityOnboardingBinding
 import com.youverify.agent_app_android.databinding.OnboardingItemsBinding
 import com.youverify.agent_app_android.model.OnBoardingItems
+import com.youverify.agent_app_android.view.activity.MainActivity
+import com.youverify.agent_app_android.view.activity.OnBoardingItemsActivity
 
-class OnBoardingAdapter(private val onboardingItems: ArrayList<OnBoardingItems>) :
+class OnBoardingAdapter(private val onboardingItems: ArrayList<OnBoardingItems>, var context:Context) :
     RecyclerView.Adapter<OnBoardingAdapter.OnBoardingViewHolder>(){
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OnBoardingViewHolder {
@@ -31,6 +34,7 @@ class OnBoardingAdapter(private val onboardingItems: ArrayList<OnBoardingItems>)
         return onboardingItems.size
     }
 
+
     inner class OnBoardingViewHolder(private val binding: OnboardingItemsBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
@@ -44,27 +48,37 @@ class OnBoardingAdapter(private val onboardingItems: ArrayList<OnBoardingItems>)
 
             //change the various levels for the progressbar on different items
             when(item){
-                onboardingItems[0] -> {
-                    binding.onBoardProgressBar.progress = 25
-                    binding.onBoardNextButton.isClickable = false
-                }
-                onboardingItems[1] -> {
-                    binding.onBoardProgressBar.progress = 50
-                    binding.onBoardNextButton.isClickable = false
-                }
-                onboardingItems[2] -> {
-                    binding.onBoardProgressBar.progress = 75
-                    binding.onBoardNextButton.isClickable = false
-                }
-                onboardingItems[3] -> {
-                    binding.onBoardProgressBar.progress = 100
-                    binding.onBoardNextButton.isClickable = true
-                }
-                else -> {
-                    binding.onBoardProgressBar.progress = 0
-                    binding.onBoardNextButton.isClickable = false
+                onboardingItems[0] -> binding.onBoardProgressBar.progress = 25
+                onboardingItems[1] -> binding.onBoardProgressBar.progress = 50
+                onboardingItems[2] -> binding.onBoardProgressBar.progress = 75
+                onboardingItems[3] -> binding.onBoardProgressBar.progress = 100
+                else -> binding.onBoardProgressBar.progress = 0
+            }
+
+            binding.textSkip.setOnClickListener {
+                context.startActivity(Intent(context, MainActivity::class.java))
+            }
+            binding.onBoardBackButton.setOnClickListener {
+                bind(item = onboardingItems[onboardingItems.indexOf(item) - 1])
+            }
+
+            //make sure you make moving to the next screen work, and also the fixing od the onboarding items on the screen.
+            //look at click listener for skip, next and back buttons.
+            binding.onBoardNextButton.setOnClickListener{
+                setNextItem(onboardingItems.indexOf(item) + 1)
+                if(onboardingItems.indexOf(item) + 1 <= itemCount - 1){
+                    bind(item = onboardingItems[onboardingItems.indexOf(item) + 1])
+                    onBindViewHolder(this, onboardingItems.indexOf(item) + 1)
+                }else {
+                    //move to next screen
+                    context.startActivity(Intent(context, MainActivity::class.java))
                 }
             }
+        }
+
+        //method to handle next button for onboarding
+        private fun setNextItem(count: Int){
+
         }
     }
 }
--- app/src/main/java/com/youverify/agent_app_android/view/activity/OnBoardingItemsActivity.kt
+++ app/src/main/java/com/youverify/agent_app_android/view/activity/OnBoardingItemsActivity.kt
@@ -5,31 +5,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.youverify.agent_app_android.databinding.OnboardingItemsBinding
 
-class OnBoardingItemsActivity : AppCompatActivity() {
+open class OnBoardingItemsActivity : AppCompatActivity() {
     private lateinit var binding: OnboardingItemsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = OnboardingItemsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // we want to go back to the previous item on the list if we hit the back button
-        binding.onBoardBackButton.setOnClickListener {
-            // call getItem() function
-
-        }
-
-        // we want to go to the main activity if we hit the skip text
         binding.textSkip.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
+            startActivity(Intent(applicationContext, MainActivity::class.java))
             finish()
-            println("clicked skip")
         }
 
-        // we want to go to the main activity if we hit the skip text
         binding.onBoardNextButton.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
+            startActivity(Intent(applicationContext, MainActivity::class.java))
             finish()
-            println("clicked next")
         }
+    }
+
+    // we want to go to the main activity if we hit the skip text
+    fun onClickButton(){
+        startActivity(Intent(applicationContext, MainActivity::class.java))
+        finish()
     }
 }
