99f8ee5e6207039d042cf6037b313e5386c21ab1
solidtm <richardebboh@gmail.com>
2022-02-11 11:29:27 +0100

    chore: housekeeping

--- app/src/main/java/com/youverify/agent_app_android/view/fragment/onboarding/ViewPagerFragment.kt
+++ app/src/main/java/com/youverify/agent_app_android/view/fragment/onboarding/ViewPagerFragment.kt
@@ -1,13 +1,15 @@
 package com.youverify.agent_app_android.view.fragment.onboarding
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.youverify.agent_app_android.R
+import androidx.fragment.app.Fragment
 import com.youverify.agent_app_android.databinding.FragmentViewPagerBinding
-import com.youverify.agent_app_android.view.fragment.onboarding.screens.*
+import com.youverify.agent_app_android.view.fragment.onboarding.screens.FirstScreen
+import com.youverify.agent_app_android.view.fragment.onboarding.screens.FourthScreen
+import com.youverify.agent_app_android.view.fragment.onboarding.screens.SecondScreen
+import com.youverify.agent_app_android.view.fragment.onboarding.screens.ThirdScreen
 
 
 class ViewPagerFragment : Fragment() {
