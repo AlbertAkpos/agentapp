29ed38b9d52a84ece084daf61ffcaefdf61e96b0
richardmmakuaebo <mrichard@youverify.co>
2022-02-16 18:13:32 +0100

    added navigation for settings screen

--- app/src/main/java/com/youverify/agent_app_android/view/fragment/home/SettingsFragment.kt
+++ app/src/main/java/com/youverify/agent_app_android/view/fragment/home/SettingsFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.preference.PreferenceFragmentCompat
 import com.youverify.agent_app_android.R
 import com.youverify.agent_app_android.databinding.FragmentDashboardBinding
@@ -28,5 +29,13 @@ class SettingsFragment : Fragment(){
         binding = FragmentSettingsBinding.inflate(layoutInflater)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.toolbar.setNavigationOnClickListener {
+            findNavController().popBackStack()
+        }
     }
 }
