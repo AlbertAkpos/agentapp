ff4110a54cad12213721b2ea0b2be590bf699fe5
richardmmakuaebo <mrichard@youverify.co>
2022-02-16 18:00:58 +0100

    changed settings fragment to from using preference screen

--- app/src/main/java/com/youverify/agent_app_android/view/fragment/home/SettingsFragment.kt
+++ app/src/main/java/com/youverify/agent_app_android/view/fragment/home/SettingsFragment.kt
@@ -1,12 +1,32 @@
 package com.youverify.agent_app_android.view.fragment.home
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.preference.PreferenceFragmentCompat
 import com.youverify.agent_app_android.R
+import com.youverify.agent_app_android.databinding.FragmentDashboardBinding
+import com.youverify.agent_app_android.databinding.FragmentSettingsBinding
 
-class SettingsFragment : PreferenceFragmentCompat() {
+class SettingsFragment : Fragment(){
 
-    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        setPreferencesFromResource(R.xml.root_settings, rootKey)
+//    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+////        activity?.setContentView(R.layout.fragment_settings)
+//        setPreferencesFromResource(R.layout.fragment_settings, rootKey)
+//    }
+
+    private lateinit var binding: FragmentSettingsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        binding = FragmentSettingsBinding.inflate(layoutInflater)
+
+        return binding.root
     }
 }
