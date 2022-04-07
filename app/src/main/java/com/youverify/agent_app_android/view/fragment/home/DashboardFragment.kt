8660394a1ccf874cd7a91107c347956a6a9e5273
richardmmakuaebo <mrichard@youverify.co>
2022-04-07 09:34:25 +0100

    feat(onboarding): implemented verify my identity screens

--- app/src/main/java/com/youverify/agent_app_android/view/fragment/home/DashboardFragment.kt
+++ app/src/main/java/com/youverify/agent_app_android/view/fragment/home/DashboardFragment.kt
@@ -1,13 +1,19 @@
 package com.youverify.agent_app_android.view.fragment.home
 
+import android.app.AlertDialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.youverify.agent_app_android.R
 import com.youverify.agent_app_android.databinding.FragmentDashboardBinding
-import com.youverify.agent_app_android.databinding.FragmentProfileBinding
 
 class DashboardFragment : Fragment(R.layout.fragment_dashboard) {
     private lateinit var binding: FragmentDashboardBinding
@@ -17,9 +23,36 @@ class DashboardFragment : Fragment(R.layout.fragment_dashboard) {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         binding = FragmentDashboardBinding.inflate(layoutInflater)
 
+        Handler(Looper.getMainLooper()).postDelayed({
+            showCompleteOnboardingDialog()
+        }, 500)
+
         return binding.root
+    }
+
+
+    private fun showCompleteOnboardingDialog(){
+        val dialogBuilder =
+            AlertDialog.Builder(requireContext(), R.style.CustomAlertDialog).create()
+        val view = layoutInflater.inflate(R.layout.complete_onboarding_dialog, null)
+        val checkCompleteTraining = view.findViewById<CheckBox>(R.id.check_complete_training)
+        val checkVerifyIdentity = view.findViewById<CheckBox>(R.id.check_verify_identity)
+        val checkSelectPrefAreas = view.findViewById<CheckBox>(R.id.check_select_areas)
+        dialogBuilder.setView(view)
+        checkVerifyIdentity.setOnClickListener {
+            dialogBuilder.dismiss()
+            findNavController().navigate(R.id.action_dashboardFragment_to_selectIDFragment)
+        }
+
+        checkSelectPrefAreas.setOnClickListener {
+            dialogBuilder.dismiss()
+            findNavController().navigate(R.id.action_dashboardFragment_to_selectAreasFragment)
+        }
+        dialogBuilder.setCancelable(false)
+        dialogBuilder.show()
+//        dialogBuilder.window?.setLayout(1100, 1600)
+        dialogBuilder.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
     }
 }
--- app/src/main/java/com/youverify/agent_app_android/view/fragment/home/identity_verification/UploadPassportFragment.kt
+++ app/src/main/java/com/youverify/agent_app_android/view/fragment/home/identity_verification/UploadPassportFragment.kt
@@ -5,56 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.youverify.agent_app_android.R
+import com.youverify.agent_app_android.databinding.FragmentUploadPassportBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [UploadPassportFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class UploadPassportFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class UploadPassportFragment : Fragment(R.layout.fragment_upload_passport) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding : FragmentUploadPassportBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_upload_passport, container, false)
-    }
+    ): View{
+        binding = FragmentUploadPassportBinding.inflate(layoutInflater)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment UploadPassportFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            UploadPassportFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        binding.verifyBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_uploadPassportFragment_to_verificationResponseFragment)
+        }
+
+        binding.backBtn.setOnClickListener {
+            activity?.onBackPressed()
+        }
+
+        return binding.root
     }
 }
