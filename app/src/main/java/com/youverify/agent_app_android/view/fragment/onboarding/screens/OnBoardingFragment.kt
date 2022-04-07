d996d94281bff3714f5fd592d8d87f0c450dc240
richardmmakuaebo <mrichard@youverify.co>
2022-04-07 09:44:21 +0100

    feat(onboarding): implemented pop animation for viewpager

--- app/src/main/java/com/youverify/agent_app_android/view/fragment/onboarding/screens/OnBoardingFragment.kt
+++ app/src/main/java/com/youverify/agent_app_android/view/fragment/onboarding/screens/OnBoardingFragment.kt
@@ -1,4 +1,4 @@
-package com.youverify.agent_app_android.view.fragment.onboarding
+package com.youverify.agent_app_android.view.fragment.onboarding.screens
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,17 +7,18 @@ import android.view.ViewGroup
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.viewpager2.widget.ViewPager2
 import com.youverify.agent_app_android.R
 import com.youverify.agent_app_android.databinding.FragmentOnboardingBinding
 import com.youverify.agent_app_android.model.OnBoardingItem
-import kotlin.properties.Delegates
+import com.youverify.agent_app_android.view.fragment.onboarding.OnBoardingItemsAdapter
+import kotlin.math.abs
 
 
-class OnBoardingFragment : Fragment(R.layout.fragment_onboarding) {
+class OnBoardingFragment : Fragment(R.layout.fragment_onboarding), ViewPager2.PageTransformer {
 
     private lateinit var binding: FragmentOnboardingBinding
     private lateinit var onboardingItemsAdapter: OnBoardingItemsAdapter
-    private var isFromTCScreen by Delegates.notNull<Boolean>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,8 +26,9 @@ class OnBoardingFragment : Fragment(R.layout.fragment_onboarding) {
     ): View {
         binding = FragmentOnboardingBinding.inflate(layoutInflater)
 
-        binding.onBoardBackButton.isVisible = false
+        binding.onboardBackBtn.isVisible = false
         binding.viewPager.isUserInputEnabled = false
+        binding.viewPager.setPageTransformer(this)
         setOnBoardingItems()
         configureNextButton()
         configureBackButton()
@@ -36,7 +38,7 @@ class OnBoardingFragment : Fragment(R.layout.fragment_onboarding) {
 
     override fun onStop() {
         super.onStop()
-        binding.onBoardBackButton.isVisible = true
+        binding.onboardBackBtn.isVisible = true
     }
 
     //this function sets the items of the viewPager
@@ -71,20 +73,20 @@ class OnBoardingFragment : Fragment(R.layout.fragment_onboarding) {
 
     //this function listens to next button
     private fun configureNextButton() {
-        binding.onBoardNextButton.setOnClickListener {
+        binding.onboardNextBtn.setOnClickListener {
             when (binding.viewPager.currentItem) {
                 0 -> {
                     binding.viewPager.currentItem = 1
-                    binding.onBoardProgressBar.progress = 50
-                    binding.onBoardBackButton.isVisible = true
+                    binding.onboardProgressBar.progress = 50
+                    binding.onboardBackBtn.isVisible = true
                 }
                 1 -> {
                     binding.viewPager.currentItem = 2
-                    binding.onBoardProgressBar.progress = 75
+                    binding.onboardProgressBar.progress = 75
                 }
                 2 -> {
                     binding.viewPager.currentItem = 3
-                    binding.onBoardProgressBar.progress = 100
+                    binding.onboardProgressBar.progress = 100
                 }
                 else -> {
                     findNavController().navigate(R.id.action_viewPagerFragment_to_TCScreen)
@@ -95,20 +97,20 @@ class OnBoardingFragment : Fragment(R.layout.fragment_onboarding) {
 
     //this function listens to back button
     private fun configureBackButton() {
-        binding.onBoardBackButton.setOnClickListener {
+        binding.onboardBackBtn.setOnClickListener {
             when (binding.viewPager.currentItem) {
                 1 -> {
-                    binding.onBoardBackButton.isVisible = false
+                    binding.onboardBackBtn.isVisible = false
                     binding.viewPager.currentItem = 0
-                    binding.onBoardProgressBar.progress = 25
+                    binding.onboardProgressBar.progress = 25
                 }
                 2 -> {
                     binding.viewPager.currentItem = 1
-                    binding.onBoardProgressBar.progress = 50
+                    binding.onboardProgressBar.progress = 50
                 }
                 3 -> {
                     binding.viewPager.currentItem = 2
-                    binding.onBoardProgressBar.progress = 75
+                    binding.onboardProgressBar.progress = 75
                 }
             }
         }
@@ -118,6 +120,19 @@ class OnBoardingFragment : Fragment(R.layout.fragment_onboarding) {
     private fun configureSkipText() {
         binding.textSkip.setOnClickListener {
             findNavController().navigate(R.id.action_viewPagerFragment_to_TCScreen)
+        }
+    }
+
+    //pop animation for view pager
+    override fun transformPage(page: View, position: Float) {
+        page.translationX = -position * page.width
+
+                if (abs(position) < 0.5) {
+            page.visibility = View.VISIBLE
+            page.scaleX = 1 - abs(position)
+            page.scaleY = 1 - abs(position)
+        } else if (abs(position) > 0.5) {
+            page.visibility = View.GONE
         }
     }
 }
